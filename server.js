bdd2324c93cf5c07ecff367ec409bf2655c14fae
Glitch (like-summer) <none>
2019-03-09 14:58:07 +0000

    Finished password hashing exercises

--- server.js
+++ server.js
@@ -14,13 +14,21 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash); 
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); 
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash); 
 
-
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result); 
 
 //END_SYNC
 
